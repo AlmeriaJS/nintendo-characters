599a94dd2aa95b243c0276bd461e4ba0b28fb708
Antonio Genaro <antoniogenaro8@gmail.com>
2017-10-24 22:58:43 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+exports.getCharacters = getCharacters;
+
+function getCharacters() {
+  return [
+    {
+      name: 'Mario'
+    },
+    {
+      name: 'Link'
+    },
+    {
+      name: 'Samus'
+    }
+  ]
+}
